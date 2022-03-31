58dc759eeeab489a33622504320b6feca5835c08
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-31 10:17:54 +0530

    added:50% stake win or loose case

--- src/Gambling/gamblingSimulation.java
+++ src/Gambling/gamblingSimulation.java
@@ -1,18 +1,39 @@
 package Gambling;
 
+import java.util.Scanner;
+
 public class GamblingSimulation {
-	int EVERY_DAY_STAKE=100;
+	static int EVERY_DAY_STAKE=100;
     int BET_PER_DAY=1;
-	public static void main(String[] args) {
-		int WIN=0;
-		int BET= (int) Math.floor(Math.random()*10)%2;
-		if(BET==1) {
-			System.out.println("Gambler wins the game");
-		}
-		else
-		{
-			System.out.println("Gambler loose the game");
-		}
-        
-}
+    public static void main(String[]args) {
+    	int win;
+    	int winrate=0;
+    	int totalamount=100;
+    	Scanner S =new Scanner(System.in);
+    	System.out.println("enter a no of turns");
+    	int play = S.nextInt();
+    	win =(int)Math.floor(Math.random())%2;
+    	for(int i=1;i<=play;i++)
+    		if(win==1)
+    		{
+    			winrate=winrate+win;
+    			System.out.println("gambler win");
+    			totalamount=totalamount+win;
+    		}
+    		else if(win==0) {
+    			winrate=winrate-win;
+    			System.out.println("gambler loose");
+    			totalamount=totalamount-1;
+
+
+    		}
+    	if(totalamount>=50) {
+			System.out.println("gambler win"+totalamount);
+    	}
+    	else if(totalamount<=50)
+    	{
+    		System.out.println("gambler loose" +totalamount);
+    	}
+    }
+    
 }
