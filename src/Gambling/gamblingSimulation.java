07ca58bf36b63c6c54d7bd11cf23765f950fa359
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-31 08:32:31 +0530

    ADDED:variables in uc1.1

--- src/Gambling/gamblingSimulation.java
+++ src/Gambling/gamblingSimulation.java
@@ -2,12 +2,8 @@ package Gambling;
 
 public class gamblingSimulation {
 	public static void main(String[] args) {
-        int stake  = Integer.parseInt(args[0]);    
-        int goal   = Integer.parseInt(args[1]);   
-        int trials = Integer.parseInt(args[2]);    
-        int bets = 0;      
-        int wins = 0;      
-
+        int everydayStake=100;
+        int betperday=1;
       
 
 }
