8e5b66beab9814c13178ce05a046951ddc434759
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-31 17:31:46 +0530

    added:use case 7

--- src/Gambling/gamblingSimulation.java
+++ src/Gambling/gamblingSimulation.java
@@ -9,13 +9,18 @@ public class GamblingSimulation {
 		// TODO Auto-generated method stub
 	int	win;
 		int loss;
+		int result;
 		int winrate=0;
 		int totalamount=100;
+		int earnings=0;
+		int myloss=0;
 	int wonamount=0;
-	int j;
+	int totalloss=0;
+	int 	totalwon=0;
+	
 	int lostamount=0;
 		
-		for(int day=1;day<=2;day++)
+		for(int day=1;day<=20;day++)
 		{
 			
 		
@@ -37,7 +42,8 @@ public class GamblingSimulation {
 						winrate=winrate-win;
 						totalamount=totalamount-1;
 						}
-				}if(totalamount>=150) 
+				}
+				if(totalamount>=150) 
 					{
 					
 					System.out.println("gambler won the game " +totalamount);
@@ -54,8 +60,21 @@ public class GamblingSimulation {
 				{
 					System.out.println("gambler needs to play more to get the result");
 				}
+				totalwon= totalamount+totalwon;
+				earnings=earnings+wonamount;
+				myloss=myloss+lostamount;
+						
 				
-							
-				}
 	}
+		
+	if( totalwon>=3000)
+	{
+		System.out.println(" the total amount gambler won on this month is "  +totalwon+" ");
+		System.out.println(" the  amount gambler won on this month is "  +earnings+" and gambler can continue to play");
+	}
+	else
+	{
+		System.out.println("sorry  the  gambler already lost"+myloss+"and he can continue game anymore");
+	}
+}
 }
