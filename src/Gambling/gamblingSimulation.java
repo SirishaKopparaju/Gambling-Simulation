b103e6d9edb33585e9f13b6ea9b1ec37ab2d26c9
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-31 08:30:56 +0530

    Added variables in program

---
+++ src/Gambling/gamblingSimulation.java
@@ -0,0 +1,14 @@
+package Gambling;
+
+public class gamblingSimulation {
+	public static void main(String[] args) {
+        int stake  = Integer.parseInt(args[0]);    
+        int goal   = Integer.parseInt(args[1]);   
+        int trials = Integer.parseInt(args[2]);    
+        int bets = 0;      
+        int wins = 0;      
+
+      
+
+}
+}
