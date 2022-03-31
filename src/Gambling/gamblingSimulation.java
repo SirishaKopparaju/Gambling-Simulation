ca80edee8589bc1f78f59b539aa9626e5dd599f8
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-31 08:59:35 +0530

    Added:gambler bet wins or loose statement added

--- src/Gambling/gamblingSimulation.java
+++ src/Gambling/gamblingSimulation.java
@@ -4,6 +4,15 @@ public class GamblingSimulation {
 	int EVERY_DAY_STAKE=100;
     int BET_PER_DAY=1;
 	public static void main(String[] args) {
+		int WIN=0;
+		int BET=0;
+		if(BET==1) {
+			System.out.println("Gambler wins the game");
+		}
+		else
+		{
+			System.out.println("Gambler loose the game");
+		}
         
 }
 }
